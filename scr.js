d357f9781c869b7a2a3aadcc5f1a9df82f062530
Rail Rafikov <rayraf@mail.ru>
2021-04-19 12:19:41 -0700

    Добавлена функция поиска элемента по id и вывода на консоль высоты элемента

--- scr.js
+++ scr.js
@@ -10,25 +10,46 @@
        
         var list= document.getElementsByClassName("speccontainer");
             for (var i = 0; i < list.length; i++) {
-            //    list[i].style.background= "red";
-            var st = getStyles(list[i]);
-            var compst = getComputedStyle(list[i]).height;     
+                //    list[i].style.background= "red";
+                var st = getStyles(list[i]);
+                var compst = getComputedStyle(list[i]).height;     
+                console.log(compst);
 
-            console.log(compst);
-
-            //console.log(getComputedStyle(list[i]).height);
-
+                //console.log(getComputedStyle(list[i]).height);
 
 
 
-            var val = Number.parseInt(compst.replace("px",""));
-            console.log(val);
-            if(val > 677) alert("Данные не влезают на первую странцу");
+
+                var val = Number.parseInt(compst.replace("px",""));
+                console.log(val);
+                if(val > 677) alert("Данные не влезают на первую странцу");
             
 
-        }
+            }
+
+            //Выведем на консоль все id элементов
+            var i = 1;
+            while (document.getElementById('line'+i) != undefined)
+                {
+                    var ido = document.getElementById('line'+i).getAttribute('id');
+                    console.log(ido);
+                    var s = getStyles(document.getElementById('line'+i));
+                    var cs = getComputedStyle(document.getElementById('line'+i)).height;
+                    console.log(cs);
+                    i++;
+                }
+            
+
+            
+                
+                
+            
 
 
+
+            // var videoElement = document.getElementById('line1');
+            // var idOfElement = videoElement.getAttribute('id');
+            // console.log(idOfElement);
 
 
 
