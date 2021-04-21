60bdb5ddded8962a88d4675fac43713ab102f000
Rail Rafikov <rayraf@mail.ru>
2021-04-21 12:15:00 -0700

    Более или менее работающий код. Вставляет элементы из скрытого элемента в основную область учитывая получившуюся высоту

--- scr.js
+++ scr.js
@@ -28,28 +28,81 @@
             }
 
             //Выведем на консоль все id элементов и значения высоты
-            var i = 1;
-            while (document.getElementById('line'+i) != undefined)
-                {
-                    var ido = document.getElementById('line'+i).getAttribute('id');
-                    console.log(ido);
-                    var s = getStyles(document.getElementById('line'+i));
-                    var cs = getComputedStyle(document.getElementById('line'+i)).height;
-                    console.log(cs);
-                    i++;
-                }
+            // var i = 1;
+            // while (document.getElementById('line'+i) != undefined)
+            //     {
+            //         var ido = document.getElementById('line'+i).getAttribute('id');
+            //         console.log(ido);
+            //         var s = getStyles(document.getElementById('line'+i));
+            //         var cs = getComputedStyle(document.getElementById('line'+i)).height;
+            //         console.log(cs);
+            //         i++;
+            //     }
             
 
             
                 
-                
+                var list_index = 1;
+                for (var i = 1; i < arr.length+1; i++) {
+
+                    
+                    
+
+                    var clonedNode = document.getElementById('line'+i).cloneNode(true);
+                    clonedNode.id = "new_line"+i;
+                    document.querySelector('#container'+list_index).appendChild(clonedNode);   
+
+                    var cont = document.getElementById("container"+list_index);
+                    var st = getComputedStyle(cont).height; 
+                    var val = Number.parseInt(st.replace("px",""));
+                        if(val>677)
+                            {
+                                document.getElementById('new_line'+i).remove();
+                                list_index++;
+                                i--;
+                            }
+
+
+
+
+                    console.log("подсчитано:"+st);
+
+                }
             
+
+
+                
+
+
+
+
+
+
+
+
+
 
 
 
             // var videoElement = document.getElementById('line1');
             // var idOfElement = videoElement.getAttribute('id');
             // console.log(idOfElement);
+
+
+
+            //   var  i=1;
+            // while (document.getElementById('line'+i) != undefined)
+            // {
+            //     var clonedNode = document.getElementById("line"+i).cloneNode(true);
+            //     document.querySelector(".speccontainer").appendChild(clonedNode);     
+
+            // }
+
+               
+
+
+
+
 
 
 
