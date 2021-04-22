8f3d7d1e5ae9d6f281ea82d6efae4255c8ed2590
Раиль Рафиков <rayraf@LIVE.RU>
2021-04-22 19:06:27 +0500

    Листы добавляются столько, сколько нужно

--- scr.js
+++ scr.js
@@ -43,10 +43,24 @@
             
                 
                 var list_index = 1;
+                var max_height = 677;
                 for (var i = 1; i < arr.length+1; i++) {
 
-                    
-                    
+
+                    if(i==1)
+                    {
+                        var cloned_fp = document.getElementById('fp0').cloneNode(false);
+                        cloned_fp.id = 'fp'+list_index;
+                        document.querySelector('body').appendChild(cloned_fp);  
+
+                        var cloned_cont = document.getElementById('container01').cloneNode(true);
+                        cloned_cont.id = 'container'+list_index;                        
+                        document.querySelector('#fp'+list_index).appendChild(cloned_cont);   
+                    }
+
+
+
+
 
                     var clonedNode = document.getElementById('line'+i).cloneNode(true);
                     clonedNode.id = "new_line"+i;
@@ -55,11 +69,24 @@
                     var cont = document.getElementById("container"+list_index);
                     var st = getComputedStyle(cont).height; 
                     var val = Number.parseInt(st.replace("px",""));
-                        if(val>677)
+                        if(val>max_height)
                             {
                                 document.getElementById('new_line'+i).remove();
                                 list_index++;
                                 i--;
+                                //var num = list_index + 1;
+
+                                var cloned_np = document.getElementById('np0').cloneNode(false);
+                                cloned_np.id = 'np'+list_index;
+                                document.querySelector('body').appendChild(cloned_np);  
+
+                                var cloned_cont = document.getElementById('container01').cloneNode(true);
+                                
+                                cloned_cont.id = 'container' + list_index;                        
+                                document.querySelector('#np'+ list_index).appendChild(cloned_cont);   
+
+
+                                max_height=795;
                             }
 
 
